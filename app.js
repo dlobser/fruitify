dd422b485eac1a97ef745376e4340156572eeb78
Karen <karenpenglabs@gmail.com>
2015-01-17 17:31:03 -0500

    migrate to node and add database

---
+++ app.js
@@ -0,0 +1,147 @@
+var express = require('express'); //
+var ejs = require('ejs'); //embedded javascript template engine
+var app = express();
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var config = require('./server/config.json');
+var Record = require('./server/model.js');
+
+// var Auth = (require('./lib/auth.js')).Auth;
+// var Models = (require('./lib/models.js')).Models;
+
+app.set('views', __dirname);
+
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/public'));
+/*********** END SERVER CONFIGURATION *****************/
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Listening on ' + port);
+});
+
+mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  // yay!
+  console.log('yay! database connected');
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+app.get('/', function (req, res) {
+  // if (!isLoggedIn(req.session)) {
+  //   return res.redirect('/login');
+  // } else {
+  //   return res.redirect('/turbine');
+  // }
+  res.render('index.html');
+});
+
+app.post('/upload', function (req, res) {
+
+  //console.log(req.body.data, req.body.imgURL);
+
+  if (!req.body.data) {
+    return res.send('need data');
+  }
+  if (!req.body.imgURL) {
+    return res.send('need imgURL');
+  }
+
+  var record = new Record();
+  record.data = req.body.data;
+  record.imgURL = req.body.imgURL;
+  record.save();
+  //res.send('saved');
+});
+
+app.get('/gallery', function (req, res) {
+  var query = {};
+  var select = 'data imgURL';
+  var option = {
+    limit: 1
+  };
+  Record.find(query, select, option, function (err, data) {
+      if (err) return console.error(err);
+      res.send(data);
+    })
+    //res.render('gallery.html');
+});
+// main page - display the card form
+// app.get('/turbine', function (request, response) {
+
+//   response.render("turbine.html");
+// });
+
+// app.get('/login', function (req, res) {
+//   auth = new Auth;
+//   return auth.login(function (error, callback) {
+//     req.session.oauth_token = callback.oauth_token;
+//     req.session.oauth_token_secret = callback.oauth_token_secret;
+//     return res.redirect(callback.url);
+//   });
+// });
+
+// app.get('/callback', function (req, res) {
+//   auth = new Auth;
+//   return auth.handleCallback(req.query.oauth_token, req.session.oauth_token_secret, req.query.oauth_verifier, function (callback) {
+//     req.session.oauth_access_token = callback.oauth_access_token;
+//     req.session.oauth_access_token_secret = callback.oauth_access_token_secret;
+//     return res.redirect('/turbine');
+//   });
+// });
+
+// app.post('/upload', function (req, res) {
+
+//   var fs = require('fs');
+//   var buffer = new Buffer(req.body.file);
+//   fs.writeFile("./static/tree.obj", req.body.file, function (err) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(req.body);
+//       return models.putModel("./static/tree.obj", req.session.oauth_access_token, req.session.oauth_access_token_secret, function (callback) {
+//         return res.render('upload_success.html', {
+//           "callback": JSON.parse(callback),
+//           "server": cfg.API_SERVER
+//         });
+//       });
+//     }
+//   });
+// });
+
+// /* Start the App
+//  */
+// // Make server turn on and listen at defined PORT (or port 3000 if is not defined)
+// // var port = 3000;
+// // app.listen(port, function() {
+// //   console.log("Listening on " + port);
+// // });
+
+// isLoggedIn = function (session) {
+//   if (!session.oauth_access_token) {
+//     return false;
+//   }
+//   return true;
+// };
+
+// isJson = function (url) {
+//   var json;
+//   json = false;
+//   if ((url.substring(url.length - 5)) === ".json") {
+//     json = true;
+//   }
+//   return json;
+// };
