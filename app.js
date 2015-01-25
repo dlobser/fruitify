72d4f34afe1038276219330aef6b814227b6a19d
Karen <karenpenglabs@gmail.com>
2015-01-25 17:32:23 -0500

    everything before using other oauth method

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ var routes = require('./routes/index.js');
 var auth = require('./routes/auth.js');
 var mongoose = require('mongoose');
 
+var http = require('http');
+var path = require('path');
+var shapeways = require('shapeways');
+
 app.set('views', __dirname + '/views');
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
 var bodyParser = require('body-parser');
+// create application/json parser
+var jsonParser = bodyParser.json();
 app.use(bodyParser.urlencoded({
   limit: '50mb',
   extended: true
@@ -24,10 +30,13 @@ app.use(bodyParser.urlencoded({
 // }));
 var session = require('express-session');
 app.use(session({
-  secret: config.app.secret,
+  secret: 'shapeways',
   resave: false,
   saveUninitialized: true
 }))
+
+//app.use(express.methodOverride());
+app.use(auth.middleware);
 
 var port = process.env.PORT || config.port;
 app.listen(port, function () {
@@ -44,7 +53,7 @@ db.once('open', function callback() {
 
 app.get('/', routes.index);
 
-app.post('/upload', routes.uploadThumbnail);
+app.post('/uploadThumbnail', routes.uploadThumbnail);
 
 app.get('/gallery', routes.gallery);
 
@@ -54,34 +63,12 @@ app.get('/login', auth.login);
 
 app.get('/callback', auth.callback);
 
-app.get('/:id/shape', routes.shapeRebuild);
+app.post('/uploadModel', auth.uploadModel);
+
+app.get('/shape/:id', routes.shapeRebuild);
 
 //this should always be in the bottom of this file
 app.get('/:id', routes.shapeShow);
-
-// main page - display the card form
-// app.get('/turbine', function (request, response) {
-
-//   response.render("turbine.html");
-// });
-
-// app.get('/login', function (req, res) {
-//   auth = new Auth;
-//   return auth.login(function (error, callback) {
-//     req.session.oauth_token = callback.oauth_token;
-//     req.session.oauth_token_secret = callback.oauth_token_secret;
-//     return res.redirect(callback.url);
-//   });
-// });
-
-// app.get('/callback', function (req, res) {
-//   auth = new Auth;
-//   return auth.handleCallback(req.query.oauth_token, req.session.oauth_token_secret, req.query.oauth_verifier, function (callback) {
-//     req.session.oauth_access_token = callback.oauth_access_token;
-//     req.session.oauth_access_token_secret = callback.oauth_access_token_secret;
-//     return res.redirect('/turbine');
-//   });
-// });
 
 // app.post('/upload', function (req, res) {
 
@@ -101,27 +88,3 @@ app.get('/:id', routes.shapeShow);
 //     }
 //   });
 // });
-
-// /* Start the App
-//  */
-// // Make server turn on and listen at defined PORT (or port 3000 if is not defined)
-// // var port = 3000;
-// // app.listen(port, function() {
-// //   console.log("Listening on " + port);
-// // });
-
-// isLoggedIn = function (session) {
-//   if (!session.oauth_access_token) {
-//     return false;
-//   }
-//   return true;
-// };
-
-// isJson = function (url) {
-//   var json;
-//   json = false;
-//   if ((url.substring(url.length - 5)) === ".json") {
-//     json = true;
-//   }
-//   return json;
-// };
---
+++ public/request/request.js
@@ -0,0 +1,62 @@
+function saveToDb() {
+  if (document.getElementById('thumbnail') !== null) {
+    document.getElementById('container').removeChild(document.getElementById('thumbnail'));
+  }
+  $.ajax({
+    url: '/uploadThumbnail',
+    method: 'POST',
+    data: {
+      'data': writeData(),
+      'imgURL': makeThumbnail()
+    },
+    dataType: 'json',
+    error: function (err) {
+      console.error(err);
+    },
+    success: function (data) {
+      console.log(data);
+    }
+  })
+}
+
+function askForRebuildShape(id) {
+  $.ajax({
+    url: '/shape/' + id,
+    method: 'GET',
+    dataType: 'json',
+    error: function (err) {
+      console.error(err);
+    },
+    success: function (data) {
+      console.log(data[0].data);
+      setTimeout(function () {
+        readData(JSON.parse(data[0].data));
+      }, 1000);
+    }
+  })
+}
+
+function sendToSW() {
+  $.ajax({
+    //url: 'http://api.shapeways.com/models/v1/' + data,
+    url: '/uploadModel',
+    method: 'POST',
+    dataType: 'json',
+    data: {
+      'str': outputString()
+    },
+    error: function (err) {
+      console.error(err);
+    },
+    success: function (data) {
+      console.log(data);
+    }
+  })
+}
+
+$(document).ready(function () {
+  var id = document.location.pathname.slice(1);
+  if (id.length) {
+    askForRebuildShape(id);
+  }
+});
